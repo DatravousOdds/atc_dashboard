6f26a5306444402123107b8a2dd75982ab39dc0f
Datravous Odds <datravousodds@Mac.attlocal.net>
2025-12-22 16:57:31 -0600

    added production variable app is live testing

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.use(express.static('public'));
 
 // PostgreSQL Connection
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-    // connectionString: process.env.DATABASE_URL,
-    // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 
