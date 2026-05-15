fbfb45b0631dc3128251e413df99a8175ef02129
Datravous Odds <datravousodds@gmail.com>
2026-05-15 16:48:15 -0500

    feat: Enhance project search functionality and styles; add dynamic dropdown for project selection

--- public/js/workOrders.js
+++ public/js/workOrders.js
@@ -1,4 +1,52 @@
 const workOrderDropdown = document.getElementById("wo-project-dropdown");
+const searchBtn = document.getElementById("searchBtn");
+const workOrderProjectDropdown = document.querySelector(".wo-project-dropdown");
+const searchInput = document.querySelector(".search-input");
+const projectSearch = document.querySelector(".project-search");
+const projectSelectContainer = document.querySelector(".project-select-wrapper");
+const projectNameDisplay = document.querySelector(".project-name");
+const activeCount = document.getElementById("active-count");
+
+
+
+projectSelectContainer.addEventListener("click", (e) => {
+   workOrderProjectDropdown.classList.toggle("active");
+
+   if (workOrderProjectDropdown.classList.contains("active")) {
+       const allProjects = document.querySelectorAll(".wo-project-dropdown li");
+       allProjects.forEach(project => {
+           project.addEventListener("click", () => {
+               const selectedProject = project.innerText;
+               projectNameDisplay.innerText = selectedProject;
+               workOrderProjectDropdown.classList.remove("active");
+           })
+       })
+   }
+
+})
+
+document.addEventListener("click", (event) => {
+    const target = event.target;
+    if (!target.closest(".wo-project-dropdown") && !target.closest(".project-select-wrapper")) {
+        workOrderProjectDropdown.classList.remove("active");
+    }
+
+    if (!projectSearch.contains(event.target)) {
+        searchInput.classList.remove("active");
+        projectSearch.classList.remove("active");
+    }
+
+});
+
+
+
+
+
+searchBtn.addEventListener("click", () => {
+    searchInput.classList.toggle("active");
+    projectSearch.classList.toggle("active");
+    searchInput.focus();
+});
 
 
 async function getContracts() {
